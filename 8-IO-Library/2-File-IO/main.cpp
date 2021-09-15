22e4e94bca3ad00eb7b081f37029bcde1cc8b8b9
azhong <zczwzw@163.com>
2021-09-15 23:16:05 +0800

    file io

---
+++ 8-IO-Library/2-File-IO/main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char *file = "test.txt";
+    ifstream in(file);
+
+    ifstream myfile("data1.txt");
+
+    return 0;
+}
+
