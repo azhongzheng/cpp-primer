9c0ac5fde0b808891aced85118115fab163c41fc
azhong <zczwzw@163.com>
2021-09-18 22:47:38 +0800

    add string opt

--- 3-String-Vector-Array/2-String/string_opt.cpp
+++ 3-String-Vector-Array/2-String/string_opt.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <assert.h>
 
 void test1()
 {
@@ -24,10 +25,36 @@ void test1()
     std::cout << s2.size() << "\n";
 }
 
+void test2()
+{
+    std::string::size_type a;
+    auto len = std::string("10").size();
+}
+
+void test3()
+{
+    std::string str = "Hello";
+    std::string phrase = "Hello world";
+    std::string slang = "Hiya";
+    assert(str < phrase);
+}
+void test4()
+{
+    std::string st1(10, 'c'), st2;
+    st1 = st2;
+    std::string s1 = "hello", s2 = "world";
+    std::string s3 = s1 + "," + s2 + '\n';
+    std::cout << s3;
+    std::string s4 = s1 + ",";
+    // std::string s5 = "hello" + ", "; // 不能把字面值直接相加
+    std::string s6 = s1 + ", " + "world" + "hello";
+    // std::string s7 = "hello" + ", " + s2;
+    
+}
 
 int main(int argc, char const *argv[])
 {
 
-    test1();
+    test4();
     return 0;
 }
