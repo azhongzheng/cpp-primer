d6425cca307f75a5ac7bbb9457899f328eeacfbc
azhong <zczwzw@163.com>
2021-09-18 22:23:21 +0800

    string read

---
+++ 3-String-Vector-Array/2-String/string_read.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void test1()
+{
+    std::string s1, s2;
+    std::cin >> s1 >> s2;
+    std::cout << s1 << " " << s2 << "\n";
+}
+
+void test2()
+{
+    std::string word;
+    while (std::cin >> word)
+    {
+        std::cout << word << "\n";
+    }
+}
+
+void test3()
+{
+    std::string line;
+    while (getline(std::cin, line))
+        std::cout << line << "\n";
+}
+
+int main(int argc, char const *argv[])
+{
+    // test1();
+    // test2();
+    test3();
+    return 0;
+}
