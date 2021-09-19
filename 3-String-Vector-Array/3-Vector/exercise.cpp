9340ae6dd0fca9eb20e6444ccb13909de75c6d23
azhong <zczwzw@163.com>
2021-09-19 09:48:32 +0800

    exercise

--- 3-String-Vector-Array/3-Vector/exercise.cpp
+++ 3-String-Vector-Array/3-Vector/exercise.cpp
@@ -3,14 +3,26 @@
 
 using namespace std;
 
-
-void test1(){
-    std::string word;
-    while (std::cin >> word)
+void test1()
+{
+    string word;
+    vector<string> words;
+    //mac 文件结束符 control+d
+    while (cin >> word)
+    {
+        for (auto &c : word)
+        {
+            c = toupper(c);
+        }
+        words.push_back(word);
+    }
+    for (auto word : words)
+        cout << word << " ";
 }
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    test1();
     return 0;
 }
