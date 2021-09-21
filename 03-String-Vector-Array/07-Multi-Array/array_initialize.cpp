b7adda23dc64ce316039b4107c660f144730eaba
azhong <zczwzw@163.com>
2021-09-21 14:01:11 +0800

    multi array initialize

---
+++ 03-String-Vector-Array/07-Multi-Array/array_initialize.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void test1()
+{
+    int ia1[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}};
+    int ia2[2][3] = {1, 2, 3, 4, 5, 6};
+
+    // int ia3[2][3] = {
+    //     {1, 2}, {3, 4}, {5, 6}};
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test1();
+    return 0;
+}
