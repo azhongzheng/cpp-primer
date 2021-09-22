aa412775c0486b576622881401b53df2f9fa5f22
azhong <zczwzw@163.com>
2021-09-22 23:37:55 +0800

    read file to buff

--- 08-IO-Library/02-File-IO/file_read_str.cpp
+++ 08-IO-Library/02-File-IO/file_read_str.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <string.h>
 
 using namespace std;
 
@@ -59,20 +60,35 @@ void test3()
         cout << word << " length: " << word.size() << '\n';
 }
 
-void test4(){
+void test4()
+{
     // 一次性读取整个文件
     ifstream fin;
     fin.open("Pride and Prejudice.txt", ios::in);
     stringstream buf;
-    buf << fin.rdbuf(); 
+    buf << fin.rdbuf();
     string str = buf.str();
+    cout << str << "\n";
+}
+
+void test5()
+{
+    //
+    ifstream fin("Pride and Prejudice.txt");
+    char buf[20];
+    string str;
+    //每次从fin流读取一个单词到buf，buf空间大小需要大于单词长度
+    while (fin >> buf)
+    {
+        str += strcat(buf, " ");
+    }
     cout << str << "\n";
 }
 
 int main(int argc, char const *argv[])
 {
 
-    test4();
+    test5();
 
     return 0;
 }
