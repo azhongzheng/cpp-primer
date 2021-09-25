0091e4b0931d1c13f202ed42d68d0725d6a5ca2a
azhong <zczwzw@163.com>
2021-09-25 22:43:49 +0800

    modified

--- 17-Standard-Library-Facility/4-Regex/regex_iterator.cpp
+++ 17-Standard-Library-Facility/4-Regex/regex_iterator.cpp
@@ -13,14 +13,17 @@ void test1()
     while (getline(file, line))
     {
         auto iter_begin = sregex_iterator(line.begin(),
-                                         line.end(),
-                                         word_regex);
+                                          line.end(),
+                                          word_regex);
 
         auto iter_end = sregex_iterator();
-        for(auto iter = iter_begin; iter != iter_end; iter++){
+        for (auto iter = iter_begin; iter != iter_end; iter++)
+        {
+            word_count++;
             cout << iter->str() << endl;
         }
     }
+    cout << "It contains " << word_count << " words" << endl;
 }
 
 int main(int argc, char const *argv[])
