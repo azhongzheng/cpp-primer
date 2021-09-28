1f1c65723e2a761dd83565f61747d814313d7b9d
azhong <zczwzw@163.com>
2021-09-28 23:26:51 +0800

    add file lines

---
+++ 08-IO-Library/03-File-Info/flielines.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+static const char *filepath = "/Users/azhong/Documents/Project/python-examples/File/file_split/The_Great_Qin_Empire_Bigsize.txt";
+
+void test1()
+{
+    // ifstream fin("content.txt");
+    ifstream fin(filepath);
+    string line;
+    long lines_cnt = 0;
+    while (getline(fin, line))
+    {
+        lines_cnt++;
+    }
+    cout << "line cnt: " << lines_cnt << endl;
+    //test1 time :1102.91
+}
+
+void test2()
+{
+    FILE *fp = fopen(filepath, "r");
+    fseek(fp, 0, 0);
+    long i = 0;
+    //long lg=ftell(fp);
+    char c;
+    while ((c = fgetc(fp)) != EOF)
+    {
+
+        if (c == '\n')
+            i++;
+    }
+    cout << "line cnt: " << i << endl;
+    //test2 time :1055.82
+}
+
+void test3()
+{
+    FILE *fp = fopen(filepath, "r");
+    long i = 0;
+    char line[256] = {0};
+    while (fgets(line, 255, fp))
+    {
+        i++;
+    }
+    cout << "line cnt: " << i << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    // test1();
+    clock_t start_time = clock();
+    test2();
+    clock_t end_time = clock();
+    cout << "test2 time :" << (double)(end_time - start_time) / CLOCKS_PER_SEC << endl;
+
+    start_time = clock();
+    test2();
+    end_time = clock();
+    cout << "test2 time :" << (double)(end_time - start_time) / CLOCKS_PER_SEC << endl;
+
+    start_time = clock();
+    test3();
+    end_time = clock();
+    cout << "test3 time :" << (double)(end_time - start_time) / CLOCKS_PER_SEC << endl;
+    return 0;
+}
