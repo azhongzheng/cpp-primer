cf5f862c4deb9b57be7bb34ffdd8bcfc0a330d1e
azhong <zczwzw@163.com>
2021-10-01 23:57:58 +0800

    add stringstream read

---
+++ 08-IO-Library/04-String-Stream/read.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+struct PersonInfo
+{
+    string name;
+    vector<string> phones;
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    string line, word;
+    vector<PersonInfo> people;
+    while (getline(cin, line))
+    {
+        PersonInfo info;
+        istringstream record(line);
+        record >> info.name;
+        while (record >> word)
+        {
+            info.phones.push_back(word);
+        }
+        people.push_back(info);
+    }
+    for (auto i : people)
+    {
+        for(auto p: i.phones){
+           cout << i.name << ":" << p << endl;
+        }
+    }
+    return 0;
+}
