d92aee646a9503b91c91a5f21595d95fa639258a
azhong <zczwzw@163.com>
2021-10-04 14:26:56 +0800

    add test

--- 04-Expression/08-Bit-Operator/bit_basic.cpp
+++ 04-Expression/08-Bit-Operator/bit_basic.cpp
@@ -2,8 +2,6 @@
 #include <bitset>
 using namespace std;
 
-
-
 void bit_move()
 {
     unsigned char bits = 0223;
@@ -21,28 +19,38 @@ void bit_reverse()
     cout << bitset<8>(~bits) << endl;
 }
 
-void bit_logic_opt(){
+void bit_logic_opt()
+{
     unsigned char b1 = 0145;
     unsigned char b2 = 0257;
-    // cout.width(6);
-    
- 
     cout << "b1: " << bitset<8>(b1) << endl;
 
     cout << "b2: " << bitset<8>(b2) << endl;
-    
-    cout << "b1&b2: " << bitset<8>(b1&b2) << endl;
-    cout << "b1|b2: " << bitset<8>(b1|b2) << endl;
-    cout << "b1^b2: " << bitset<8>(b1^b2) << endl;
+
+    cout << "b1&b2: " << bitset<8>(b1 & b2) << endl;
+    cout << "b1|b2: " << bitset<8>(b1 | b2) << endl;
+    cout << "b1^b2: " << bitset<8>(b1 ^ b2) << endl;
 }
 
-
+void test1()
+{
+    unsigned long quiz1 = 0;
+    // 第27位学生通过了测试
+    quiz1 |= 1UL << 27;
+    cout << "quiz1: " << bitset<32>(quiz1) << endl;
+    // 将27位学生成绩改回去
+    quiz1 &= ~(1UL << 27);
+    cout << "quiz1: " << bitset<32>(quiz1) << endl;
+    // 查看第27位学生成绩是否通过
+    bool  status = quiz1 & (1UL<<27);
+    cout << status << endl;
+}
 
 int main(int argc, char const *argv[])
 {
     /* code */
 
     // bit_reverse();
-    bit_logic_opt();
+    test1();
     return 0;
 }
