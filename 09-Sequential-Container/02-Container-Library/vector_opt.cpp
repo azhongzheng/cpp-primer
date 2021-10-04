a5cb3288d69ce0f26494af279bda151f1a464529
azhong <zczwzw@163.com>
2021-10-05 00:22:15 +0800

    add vector operator

---
+++ 09-Sequential-Container/02-Container-Library/vector_opt.cpp
@@ -0,0 +1,56 @@
+#include <list>
+#include <vector>
+#include <iostream>
+#include <string>
+
+#define VNAME(value) (#value)
+
+using namespace std;
+
+template <typename T>
+void print(vector<T> &vec)
+{
+    cout << VNAME(vec) << ": [";
+    for (auto it = vec.begin(); it != vec.end(); it++)
+    {
+        if (it == (vec.end() - 1))
+            cout << *it;
+        else
+            cout << *it << ",";
+    }
+    cout << "]\n";
+}
+
+void test1()
+{
+    vector<int> vec = {1, 4, 3, 3, 2, 2, 3};
+    cout << "vec max size: "<< vec.max_size() << endl;;
+    cout << "vec size: "<< vec.size() << endl;
+    cout << "vec empty: "<< vec.empty() << endl;
+    print(vec);
+    
+}
+
+void test2(){
+    vector<int> vec = {1, 4, 3, 3, 2, 2, 3};
+    for (auto iter = vec.begin(); iter != vec.end();)
+    {
+        if (*iter==3)
+        {
+            iter = vec.erase(iter);
+        }
+        else{
+            iter++;
+        }
+    }
+    print(vec);
+    vec.erase(vec.begin(), vec.end());
+    print(vec);
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test2();
+    return 0;
+}
