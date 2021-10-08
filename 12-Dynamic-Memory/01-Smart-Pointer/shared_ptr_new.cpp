83eb752d4134313b67ee5d090132d33fe4b90001
azhong <zczwzw@163.com>
2021-10-08 20:21:46 +0800

    add

--- 12-Dynamic-Memory/01-Smart-Pointer/shared_ptr_new.cpp
+++ 12-Dynamic-Memory/01-Smart-Pointer/shared_ptr_new.cpp
@@ -6,7 +6,7 @@ void test1()
     std::shared_ptr<int> p1(new int(1024));
     std::cout << *p1 << std::endl;
 
-    int* rp = new int(100);
+    int *rp = new int(100);
     std::shared_ptr<int> p2(rp);
 }
 
@@ -17,7 +17,8 @@ std::shared_ptr<int> clone(int p)
     // return std::make_shared<int>(p);
 }
 
-void test2(){
+void test2()
+{
     auto p1 = clone(4);
     std::cout << p1.use_count() << std::endl;
     auto p2 = p1;
@@ -26,12 +27,28 @@ void test2(){
     std::cout << p1.use_count() << std::endl;
 }
 
+void test3()
+{
+    std::shared_ptr<int> p(new int(42));
+    int *q = p.get();
+    // std::cout << p.use_count();
+    // {
+    //     std::shared_ptr<int>(q);
+    // }
+    int foo = *p;
+    std::cout << "*p: " << *p << std::endl;
+    p.reset(new int(1024));
+    std::cout << "*p: " << *p << std::endl;
+    std::cout << "p.unique: " << p.unique() << std::endl;
+    p.reset();
+    std::cout << "p.use_count: " << p.use_count() << std::endl;
+    std::cout << "p.unique: " << p.unique() << std::endl;
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    test2();
-    
-    
+    test3();
 
     return 0;
 }
