4879db24b2ddba9c16fadf6393682ba41d4024d2
azhong <zczwzw@163.com>
2021-10-12 23:32:20 +0800

    commit

--- 12-Dynamic-Memory/02-Dynamic-Array/new_array.cpp
+++ 12-Dynamic-Memory/02-Dynamic-Array/new_array.cpp
@@ -8,8 +8,8 @@ void test1()
     std::cout << sizeof(p) << std::endl;
     std::cout << pia[8] << std::endl;
 
-    delete []pia;
-    delete []p;
+    delete[] pia;
+    delete[] p;
 }
 
 void test2()
@@ -23,29 +23,41 @@ void test2()
     int *pia3 = new int[10]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::string *psa3 = new std::string[10]{"a", "b", "the", std::string(3, 'x')};
     std::cout << psa3[3] << std::endl;
-    
-    delete []pia;
-    delete []pia2;
-    delete []psa;
-    delete []psa2;
-    delete []pia3;
-    delete []psa3;
 
+    delete[] pia;
+    delete[] pia2;
+    delete[] psa;
+    delete[] psa2;
+    delete[] pia3;
+    delete[] psa3;
 }
 
-void test3(){
+void test3()
+{
     std::unique_ptr<int[]> up(new int[10]);
-    for(int i =0; i<10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         up[i] = i;
     }
     std::cout << up[6] << std::endl;
     up.release();
+}
+void test4()
+{
+    std::shared_ptr<int> sp(new int[10], [](int *p)
+                            { delete[] p; });
+    for (size_t i = 0; i < 10; i++)
+    {
+        *(sp.get() + i) = i;
+    }
+
+    sp.reset();
 }
 
 int main(int argc, char const *argv[])
 {
     /* code */
     // test1();
-    test3();
+    test4();
     return 0;
 }
