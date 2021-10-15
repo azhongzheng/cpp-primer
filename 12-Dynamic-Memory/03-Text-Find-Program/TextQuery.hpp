350ccf39ba6d794ad1a4ec1100b513e6fa0e6af7
azhong <zczwzw@163.com>
2021-10-15 22:56:19 +0800

    complete TextQuery

--- 12-Dynamic-Memory/03-Text-Find-Program/TextQuery.hpp
+++ 12-Dynamic-Memory/03-Text-Find-Program/TextQuery.hpp
@@ -39,3 +39,46 @@ TextQuery::TextQuery(std::ifstream &is) : file(new std::vector<std::string>)
         }
     }
 }
+
+class QueryResult
+{
+    friend std::ostream &print(std::ostream &, const QueryResult &);
+
+public:
+    using line_no = std::vector<std::string>::size_type;
+    QueryResult(std::string s,
+                std::shared_ptr<std::set<line_no>> p,
+                std::shared_ptr<std::vector<std::string>> f);
+
+private:
+    std::string sought;
+    std::shared_ptr<std::set<line_no>> lines;
+    std::shared_ptr<std::vector<std::string>> file;
+};
+
+QueryResult TextQuery::query(const std::string &sought) const
+{
+    static std::shared_ptr<std::set<line_no>> nodata(new std::set<line_no>);
+    auto loc = wm.find(sought);
+    if (loc == wm.end())
+        return QueryResult(sought, nodata, file);
+    else
+        return QueryResult(sought, loc->second, file);
+}
+
+inline std::string make_plural(size_t ctr, const std::string &word,
+                               const std::string &ending)
+{
+    return (ctr > 1) ? word + ending : word;
+}
+
+std::ostream &print(std::ostream &os, const QueryResult &qr)
+{
+    os << qr.sought << " occurs"
+       << qr.lines->size() << " "
+       << make_plural(qr.lines->size(), "time", "s")
+       << std::endl;
+    for (auto num : *qr.lines)
+        os << "\t" << *(qr.file->begin() + num) << std::endl;
+    return os;
+}
