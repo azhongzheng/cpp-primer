0bd3cfcf0ea3d0d0cd49dba5074864e70866a582
azhong <zczwzw@163.com>
2021-10-19 23:29:50 +0800

    add capture

--- 10-Generic-Algorithm/03-Custom-Operation/lambda_advance.cpp
+++ 10-Generic-Algorithm/03-Custom-Operation/lambda_advance.cpp
@@ -1,18 +1,90 @@
 #include <iostream>
+#include <vector>
 
 void test1()
 {
-    size_t v1 = 42;
-    auto f = [v1]
-    { return v1; };
+    //值捕获
+    int v1 = 42;
+    auto f = [v1]()
+    {
+        return v1;
+    };
+    v1 = 0;
     std::cout << f() << std::endl;
 }
 
+void test2()
+{
+    //值捕获
+    size_t v1 = 42;
+    auto f = [&v1]()
+    { return v1; };
+    // v1 = 0;
+    v1 = 0;
+    auto j = f();
+    std::cout << j << std::endl;
+}
+
+void biggies(std::vector<std::string> &words,
+             std::vector<std::string>::size_type sz,
+             std::ostream &os = std::cout,
+             char c = ' ')
+{
+    std::stable_sort(words.begin(), words.end(),
+                     [](const std::string &a, const std::string &b)
+                     {
+                         return a.size() < b.size();
+                     });
+
+    auto wc = std::find_if(words.begin(), words.end(), [sz](const std::string &a)
+                           { return a.size() >= sz; });
+    std::cout << *wc << std::endl;
+    std::for_each(wc, words.end(), [&os, c](std::string &s)
+                  { os << s << c; });
+    putchar('\n');
+
+    std::for_each(wc, words.end(), [=, &os](std::string &s)
+                  { os << s << c; });
+    putchar('\n');
+
+    std::for_each(wc, words.end(), [&, c](std::string &s)
+                  { os << s << c; });
+
+    putchar('\n');
+}
+
+void test3()
+{
+    std::vector<std::string> words = {"Alice", "Emma", "has", "long", "flowing", "red", "hair"};
+    biggies(words, 4);
+}
+
+void test4()
+{
+    //值捕获
+    int v1 = 42;
+    auto f = [v1]() mutable
+    {
+        return ++v1;
+    };
+    v1 = 0;
+    std::cout << f() << std::endl;
+}
+
+void test5()
+{
+    //引用捕获
+    int v1 = 42;
+    auto f = [&v1]() mutable
+    {
+        return ++v1;
+    };
+    v1 = 0;
+    std::cout << f() << std::endl;
+}
 int main(int argc, char const *argv[])
 {
     /* code */
-    // test1();
-    int a[10];
-    std::cout << a[12];
+    test5();
     return 0;
 }
