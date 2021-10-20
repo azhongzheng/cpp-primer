4e64b1b52aec09738d9b7296b69489779461ed24
azhong <zczwzw@163.com>
2021-10-20 22:54:50 +0800

    commit

--- 10-Generic-Algorithm/03-Custom-Operation/param_bind.cpp
+++ 10-Generic-Algorithm/03-Custom-Operation/param_bind.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <functional>
+#include <vector>
 
 bool check_size(const std::string &s, std::string::size_type sz)
 {
@@ -15,9 +16,19 @@ void test1()
     std::cout << b1 << std::endl;
 }
 
+void test2()
+{
+    // std::string _1 = "s";
+    std::vector<std::string> words = {"Alice", "Emma", "has", "long", "flowing", "red", "hair"};
+    // int sz = 0;
+    // auto wc = std::find_if(words.begin(), words.end(), [sz](const std::string a){});
+    auto wc = std::find_if(words.begin(), words.end(), std::bind(check_size, std::placeholders::_1, 4));
+    std::cout << *wc << std::endl;
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    test1();
+    test2();
     return 0;
 }
