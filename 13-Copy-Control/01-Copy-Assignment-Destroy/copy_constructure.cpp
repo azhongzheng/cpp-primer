e0325fdc9a0014ea749cde18b951c96c0da4428b
azhong <zczwzw@163.com>
2021-10-24 00:18:07 +0800

    add assign operator

---
+++ 13-Copy-Control/01-Copy-Assignment-Destroy/copy_constructure.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+void test1()
+{
+    std::string dots(10, '.');                //直接初始化
+    std::string s(dots);                      //直接初始化
+    std::string s2 = dots;                    //拷贝初始化
+    std::string null_book = "9-999-9999";     //拷贝初始化
+    std::string nines = std::string(10, '9'); //拷贝初始化
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
