66db74d6540b70c2bdd5068da00b6bce66e0665a
azhong <zczwzw@163.com>
2021-10-28 02:20:50 +0800

    update format output input whitespace

--- 17-Standard-Library-Facility/05-Deep-IO-Library/format_output.cpp
+++ 17-Standard-Library-Facility/05-Deep-IO-Library/format_output.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+#include <sstream>
 
 void test1()
 {
@@ -31,18 +33,51 @@ void test2()
 
 void test3()
 {
-    // 浮点数精度格式化输出
+    // 浮点数格式化输出
     // 默认长度6位
-    std::cout << "Precision: " << std::cout.precision() << " Value: " << sqrt(2.0) << std::endl;
+    std::cout << 2.01 << std::endl;
+    std::cout << "showpoint: " << std::showpoint << 2.01 << std::noshowpoint
+              << ", noshowpoint value: " << 2.01 << std::endl;
 
+    std::cout << "Value: " << sqrt(2.0) << std::endl;
+    std::cout << "Precision: " << std::cout.precision() << ", Value: " << sqrt(2.0) << std::endl;
     std::cout.precision(12); // 指定精度长度
-    std::cout << "Precision: " << std::cout.precision() << " Value: " << sqrt(2.0) << std::endl;
+    std::cout << "Precision: " << std::cout.precision() << ", Value: " << sqrt(2.0) << std::endl;
+}
+
+
+
+void test4()
+{
+    // 输出补白
+    int i = -16;
+    double d = 3.14159;
+    std::cout << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n";
+    std::cout << std::left << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n"
+              << std::right; //正常对齐
+    std::cout << std::right << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n";
+    std::cout << std::internal << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n"
+              << std::setfill(' ');
+    std::cout << std::setfill('#') << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n"
+              << std::setfill(' ');
+
+    std::cout << std::right << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n";
+    std::cout << std::setfill('#') << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n";
+    std::cout << std::left << std::setfill('#') << "i: " << std::setw(12) << i << "next cool\n"
+              << "d: " << std::setw(12) << d << "next cool\n";
 }
 
 int main(int argc, char const *argv[])
 {
     // test1();
     // test2();
-    test3();
+    // test4();
     return 0;
 }
