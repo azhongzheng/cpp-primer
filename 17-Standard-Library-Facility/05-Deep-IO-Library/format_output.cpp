865d3599a15d3426445cbd84575cae14f7723e18
azhong <zczwzw@163.com>
2021-10-28 00:06:55 +0800

    update

--- 17-Standard-Library-Facility/05-Deep-IO-Library/format_output.cpp
+++ 17-Standard-Library-Facility/05-Deep-IO-Library/format_output.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 void test1()
 {
+    // bool值格式化输出
     std::cout << "default true: " << true << ", default false: " << false << std::endl;
     std::cout << "boolalpha true: " << std::boolalpha << true
               << ", boolalpha false: " << std::boolalpha << false << std::endl;
@@ -11,7 +13,7 @@ void test1()
 
 void test2()
 {
-
+    // 进制格式化输出
     std::cout << "default: " << 20 << " " << 1024 << std::endl;
     std::cout << "octal: " << std::oct << 20 << " " << 1024 << std::endl;
     std::cout << "hex: " << std::hex << 20 << " " << 1024 << std::endl;
@@ -27,10 +29,20 @@ void test2()
               << std::nouppercase << std::noshowbase << std::endl;
 }
 
+void test3()
+{
+    // 浮点数精度格式化输出
+    // 默认长度6位
+    std::cout << "Precision: " << std::cout.precision() << " Value: " << sqrt(2.0) << std::endl;
+
+    std::cout.precision(12); // 指定精度长度
+    std::cout << "Precision: " << std::cout.precision() << " Value: " << sqrt(2.0) << std::endl;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
-    test1();
-    test2();
+    // test1();
+    // test2();
+    test3();
     return 0;
 }
