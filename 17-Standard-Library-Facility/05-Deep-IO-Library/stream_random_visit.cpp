84459103fc01528b670edf9bd2709e34a8c4b2aa
azhong <zczwzw@163.com>
2021-10-30 23:32:36 +0800

    update

--- 17-Standard-Library-Facility/05-Deep-IO-Library/stream_random_visit.cpp
+++ 17-Standard-Library-Facility/05-Deep-IO-Library/stream_random_visit.cpp
@@ -17,7 +17,7 @@ int modify_file()
     {
         cnt += line.size() + 1;
         auto mark = inOut.tellg();         // 当前读取的位置
-        inOut.seekp(0, std::fstream::beg); //写标记移到文件末尾
+        inOut.seekp(0, std::fstream::end); //写标记移到文件末尾
         inOut << cnt;
         if (mark != end_mark)
             inOut << " ";
@@ -28,13 +28,20 @@ int modify_file()
     return EXIT_SUCCESS;
 }
 
-void test1(){
-    
+void test1()
+{
+    std::fstream inOut("test.txt", std::fstream::ate | std::fstream::in | std::fstream::out);
+    if (!inOut)
+    {
+        std::cerr << "Unable to open ifle!" << std::endl;
+    }
+    inOut.seekg(0, std::fstream::beg);
+    inOut << "nice";
 }
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    modify_file();
+    test1();
     return 0;
 }
