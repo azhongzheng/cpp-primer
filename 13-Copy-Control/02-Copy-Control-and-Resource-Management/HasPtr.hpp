a5b058fc3964ab3a2abee3b63d8cc963fd6d2ed6
azhong <zczwzw@163.com>
2021-11-12 23:22:15 +0800

    update

--- 13-Copy-Control/02-Copy-Control-and-Resource-Management/HasPtr.hpp
+++ 13-Copy-Control/02-Copy-Control-and-Resource-Management/HasPtr.hpp
@@ -12,3 +12,12 @@ private:
     std::string *ps;
     int i;
 };
+
+HasPtr &HasPtr::operator=(const HasPtr &rhs)
+{
+    auto newp = new std::string(*rhs.ps);
+    delete ps;
+    ps = newp;
+    i = rhs.i;
+    return *this;
+}
