972672dc6d65be2d5d955a81e826ed05a2065a1c
azhong <zczwzw@163.com>
2021-12-04 21:46:50 +0800

    update

---
+++ 13-Copy-Control/03-Swap/main.cpp
@@ -0,0 +1,20 @@
+#include "HasPtr.hpp"
+#include <iostream>
+#include <vector>
+
+HasPtr f(HasPtr hp)
+{
+    HasPtr ret = hp;
+    std::cout << "hp:" << &hp << "\n";
+    std::cout << "ret:" << &ret << "\n";
+    return ret;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    HasPtr hasptr = HasPtr();
+    auto f1 = f(hasptr);
+    std::vector<int> v;
+    return 0;
+}
