3360c677990becbec3fe8974346c4f4de699e3af
azhong <zczwzw@163.com>
2021-12-10 23:26:50 +0800

    update

--- 15-OOP/01-Overview/Bulk_quote.h
+++ 15-OOP/01-Overview/Bulk_quote.h
@@ -2,11 +2,21 @@
 
 class Bulk_quote : public Quote
 {
-private:
-    /* data */
 public:
-    double net_price(size_t n) const override
-    {
-        return n * 2;
-    }
+    Bulk_quote() = default;
+    Bulk_quote(const std::string &book, double p, size_t qty, double disc)
+        : Quote(book, p), min_qty(qty), discount(disc) {}
+    double net_price(size_t n) const override;
+
+private:
+    size_t min_qty = 0;
+    double discount = 0.0;
 };
+
+double Bulk_quote::net_price(size_t cnt) const
+{
+    if (cnt > min_qty)
+        return cnt * (1 - discount) * price;
+    else
+        return cnt * price;
+}
