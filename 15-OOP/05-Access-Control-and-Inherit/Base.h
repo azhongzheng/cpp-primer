2e4ae0b59d542747399f397b6c2a2bd218414ed1
azhong <zczwzw@163.com>
2022-06-23 23:34:17 +0800

    update

---
+++ 15-OOP/05-Access-Control-and-Inherit/Base.h
@@ -0,0 +1,24 @@
+
+
+class Base
+{
+public:
+    void pub_mem();
+
+protected:
+    int prot_mem();
+
+private:
+    char priv_mem;
+};
+
+struct Pub_Derv : public Base
+{
+    int f() { return prot_mem; }
+    // char g() { return priv_mem; }
+};
+
+struct Priv_Derv : private Base
+{
+    int f1() const { return prot_mem; }
+};
