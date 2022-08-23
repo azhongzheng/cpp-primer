31ee78adf22d9b2bd9503b53f9ce487a0be8e035
azhong <zczwzw@163.com>
2022-08-23 23:29:21 +0800

    sync basic

---
+++ 20-Concurrent-Programming/parallel101_course/02_sync/01/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <future>
+
+int download(std::string file) {
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Downloading " << file
+                  << " (" << i * 10 << "%)..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    }
+    std::cout << "Download complete: " << file << std::endl;
+    return 404;
+}
+
+void interact() {
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hi, " << name << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::future<int> fret = std::async([&] {
+        return download("hello.zip"); 
+    });
+    interact();
+    int ret = fret.get();
+    std::cout << "Download result: " << 0 << std::endl;
+    
+    return 0;
+}
