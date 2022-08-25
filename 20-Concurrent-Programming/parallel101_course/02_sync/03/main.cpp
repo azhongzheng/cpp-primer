0707a323b40d00c3eb26e03551d8f879ac1314f1
azhong <zczwzw@163.com>
2022-08-25 21:31:12 +0800

    sleep for

---
+++ 20-Concurrent-Programming/parallel101_course/02_sync/03/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <future>
+
+int download(std::string file) {
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Downloading " << file
+                  << " (" << i * 10 << "%)..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    }
+    std::cout << "Download complete: " << file << std::endl;
+    return 404;
+}
+
+void interact() {
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hi, " << name << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::future<int> fret = std::async([&] {
+        return download("hello.zip"); 
+    });
+    interact();
+    while (true)
+    {
+        std::cout << "Waitting for download complete..." << std::endl;
+        auto stat = fret.wait_for(std::chrono::milliseconds(1000));
+        if(stat == std::future_status::ready){
+            std::cout << "Future is ready!" << std::endl;
+            break;
+        }else{
+            std::cout << "Future not read!!!" << std::endl;
+        }
+    }
+    
+    int ret = fret.get();
+    std::cout << "Download result: " << ret << std::endl;
+    
+    return 0;
+}
